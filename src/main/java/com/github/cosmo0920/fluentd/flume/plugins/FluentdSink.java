48c9bbaf023d6f9e9b567b40091f60bc3c4c9797
Hiroshi Hatake <hatake@clear-code.com>
2015-11-26 14:04:55 +0900

    Create Fluentd logger
    TODOs:
    * Send event body into Fluentd
    * Write test code

--- src/main/java/com/github/cosmo0920/fluentd/flume/plugins/FluentdSink.java
+++ src/main/java/com/github/cosmo0920/fluentd/flume/plugins/FluentdSink.java
@@ -12,6 +12,10 @@ import org.apache.flume.sink.AbstractSink;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import org.komamitsu.fluency.Fluency;
+
+import java.io.IOException;
+
 import com.google.common.base.Preconditions;
 
 public class FluentdSink extends AbstractSink implements Configurable {
@@ -22,10 +26,19 @@ public class FluentdSink extends AbstractSink implements Configurable {
 	private static final String DEFAULT_TAG = "flume.fluentd.sink";
 
 	private String hostname;
-	private Integer port;
+	private int port;
 	private String tag;
+	private Fluency fluentLogger; // TODO: extract this member to other class for safety.
 
 	private CounterGroup counterGroup;
+
+	public Fluency setupFluentdLogger(String hostname, int port) throws  IOException {
+		return Fluency.defaultFluency(hostname, port, new Fluency.Config());
+	}
+
+	public FluentdSink() {
+		counterGroup = new CounterGroup();
+	}
 
 	public void configure(Context context) {
 		hostname = context.getString("hostname");
@@ -50,9 +63,30 @@ public class FluentdSink extends AbstractSink implements Configurable {
 		Preconditions.checkState(tag != null, "No tag specified");
 	}
 
+	private void closeFluentdLogger() {
+		if (this.fluentLogger != null) {
+			try {
+				this.fluentLogger.close();
+			} catch (IOException e) {
+				// Do nothing.
+			}
+		}
+	}
+
 	@Override
 	public void start() {
 		logger.info("Fluentd sink starting");
+
+		try {
+			this.fluentLogger = setupFluentdLogger(this.hostname, this.port);
+		} catch (IOException e) {
+			logger.error("Unable to create Fluentd logger using hostname:"
+						 + hostname + " port:" + port + ". Exception follows.", e);
+
+			closeFluentdLogger();
+
+			return; // FIXME: mark this plugin as failed.
+		}
 
 		super.start();
 
@@ -63,6 +97,8 @@ public class FluentdSink extends AbstractSink implements Configurable {
 	public void stop() {
 		logger.info("Fluentd sink {} stopping", this.getName());
 
+		closeFluentdLogger();
+
 		super.stop();
 
 		logger.debug("Fluentd sink {} stopped. Metrics:{}", this.getName(), counterGroup);
@@ -71,6 +107,39 @@ public class FluentdSink extends AbstractSink implements Configurable {
 	@Override
 	public Status process() throws EventDeliveryException {
 		Status status = Status.READY;
+		Channel channel = getChannel();
+		Transaction transaction = channel.getTransaction();
+
+		try {
+			transaction.begin();
+
+			Event event = channel.take();
+
+			if (event == null) {
+				 counterGroup.incrementAndGet("event.empty");
+				 status = Status.BACKOFF;
+			} else {
+				// TODO: send info Fluentd.
+				counterGroup.incrementAndGet("event.fluentd");
+			}
+
+			transaction.commit();
+
+		} catch (ChannelException e) {
+			transaction.rollback();
+			logger.error(
+					"Unable to get event from channel. Exception follows.", e);
+			status = Status.BACKOFF;
+		} catch (Exception e) {
+			transaction.rollback();
+			logger.error(
+					"Unable to communicate with Fluentd. Exception follows.",
+					e);
+			status = Status.BACKOFF;
+			// TODO: destroy connection to Fluentd.
+		} finally {
+			transaction.close();
+		}
 
 		return status;
 	}
