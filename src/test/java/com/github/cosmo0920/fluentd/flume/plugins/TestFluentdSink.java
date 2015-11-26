17c973d1a5aa2f8b33a94e1d5efddab689ad2386
Hiroshi Hatake <hatake@clear-code.com>
2015-11-26 18:05:30 +0900

    Add test cases for normal, un sccessful, and empty event circumstances

--- src/test/java/com/github/cosmo0920/fluentd/flume/plugins/TestFluentdSink.java
+++ src/test/java/com/github/cosmo0920/fluentd/flume/plugins/TestFluentdSink.java
@@ -7,6 +7,7 @@ import org.apache.flume.Channel;
 import org.apache.flume.Event;
 import org.apache.flume.Transaction;
 import org.apache.flume.EventDeliveryException;
+import org.apache.flume.event.EventBuilder;
 
 import org.komamitsu.fluency.Fluency;
 
@@ -25,16 +26,19 @@ import org.junit.rules.TemporaryFolder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.List;
+import java.io.IOException;
+import java.util.Map;
+import java.util.HashMap;
 
 public class TestFluentdSink {
 	private static final Logger logger = LoggerFactory.getLogger(FluentdSink.class);
 
 	@Test
-	public void testShouldCommitSuccessfulTransaction() throws EventDeliveryException {
+	public void testShouldCommitASuccessfulTransaction() throws EventDeliveryException {
 		Channel mockChannel = mock(Channel.class);
 		Transaction mockTransaction = mock(Transaction.class);
 		Event mockEvent = mock(Event.class);
+		FluencyPublisher mockFluencyPublisher = mock(FluencyPublisher.class);
 
 		when(mockChannel.getTransaction()).thenReturn(mockTransaction);
 		when(mockChannel.take()).thenReturn(mockEvent);
@@ -42,11 +46,61 @@ public class TestFluentdSink {
 
 		FluentdSink sink = new FluentdSink();
 		sink.setChannel(mockChannel);
+		sink.publisher = mockFluencyPublisher;
+		assertNotNull(sink.publisher);
 
-		// TODO: Extract Fluency sender and mock it
-		assertTrue(true);
-		// Status status = sink.process();
-		// assertEquals(status, Status.READY);
+		Status status = sink.process();
+		assertEquals(status, Status.READY);
+
+		verify(mockTransaction, times(1)).begin();
+		verify(mockTransaction, times(1)).commit();
+		verify(mockTransaction, times(1)).close();
 	}
 
+	@Test
+	public void testShouldRollbackAnUnsuccessfulTransaction() throws EventDeliveryException, IOException {
+		Channel mockChannel = mock(Channel.class);
+		Transaction mockTransaction = mock(Transaction.class);
+		Event mockEvent = mock(Event.class);
+		FluencyPublisher mockFluencyPublisher = mock(FluencyPublisher.class);
+
+		when(mockChannel.getTransaction()).thenReturn(mockTransaction);
+		when(mockChannel.take()).thenReturn(mockEvent);
+		when(mockEvent.getBody()).thenReturn("test body".getBytes(StandardCharsets.UTF_8));
+
+		FluentdSink sink = new FluentdSink();
+		sink.setChannel(mockChannel);
+		sink.publisher = mockFluencyPublisher;
+		assertNotNull(sink.publisher);
+		doThrow(new IOException()).when(mockFluencyPublisher).send(mockEvent);
+
+		Status status = sink.process();
+		assertEquals(status, Status.BACKOFF);
+
+		verify(mockTransaction, times(1)).begin();
+		verify(mockTransaction, times(1)).rollback();
+		verify(mockTransaction, times(1)).close();
+	}
+
+	@Test
+	public void testShouldRollbackWhenThereAreNoEventsInChannel() throws EventDeliveryException, IOException {
+		Channel mockChannel = mock(Channel.class);
+		Transaction mockTransaction = mock(Transaction.class);
+		FluencyPublisher mockFluencyPublisher = mock(FluencyPublisher.class);
+
+		when(mockChannel.getTransaction()).thenReturn(mockTransaction);
+		when(mockChannel.take()).thenReturn(null);
+
+		FluentdSink sink = new FluentdSink();
+		sink.setChannel(mockChannel);
+		sink.publisher = mockFluencyPublisher;
+		assertNotNull(sink.publisher);
+
+		Status status = sink.process();
+		assertEquals(status, Status.BACKOFF);
+
+		verify(mockTransaction, times(1)).begin();
+		verify(mockTransaction, times(1)).rollback();
+		verify(mockTransaction, times(1)).close();
+	}
 }
